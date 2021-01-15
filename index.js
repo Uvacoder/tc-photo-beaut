e15d05a413fae018237e0322a347a6e1ecfc0d1a
codetann <thetannerthomas@gmail.com>
2021-01-15 11:29:55 -0700

    added google contact form

--- index.js
+++ index.js
@@ -26,63 +26,6 @@ app.get("/contact", (req, res) => {
   res.sendFile(path.join(__dirname, "/public", "contact.html"));
 });
 
-app.post("/formSubmit", (req, res) => {
-  const data = req.body;
-
-  const stmpTransport = nodemailer.createTransport({
-    service: "Gmail",
-    auth: {
-      user: process.env.GMAIL_USERNAME,
-      pass: process.env.GMAIL_PASSWORD,
-    },
-  });
-
-  stmpTransport.sendMail(
-    {
-      from: "Website",
-      to: "tcphotobeauty@gmail.com",
-      subject: "New Message",
-      html: `Timestamp: ${getDate()}<br> Name: ${data.name}<br> Email: ${
-        data.email
-      }<br> Phone: ${data.phone}<br> Message: ${data.message} `,
-    },
-    (err, response) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(`message sent`);
-      }
-    },
-    stmpTransport.close()
-  );
-  res.sendFile(path.join(__dirname, "/public", "thanks.html"));
-});
-
-// function to return todays date
-function getDate() {
-  const monthNames = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  const dateObj = new Date();
-  const month = monthNames[dateObj.getMonth()];
-  const day = String(dateObj.getDate()).padStart(2, "0");
-  const year = dateObj.getFullYear();
-  const output = month + "\n" + day + "," + year;
-
-  return output;
-}
-
 // - Server - //
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`[server] running on port ${PORT}`));
